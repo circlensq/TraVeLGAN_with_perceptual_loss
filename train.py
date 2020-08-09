bb114b937f0f23418e3e99408ec0e0f42abbbc31
RichardOey <richfir@gmail.com>
2020-08-09 18:18:11 +0800

    setting cifar.json and clean the train.py code

--- train.py
+++ train.py
@@ -18,7 +18,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--log", type=str, default='./log_siamese_learn_VGG/', help="name of log folder")
 parser.add_argument("-p", "--hparams", type=str, default='cifar', help="hparams config file")
 parser.add_argument('--project_name', required=False, default='TravelGAN_siamese_learn_VGG',  help='project name')
-parser.add_argument('--input_size', type=int, default=128, help='input size')
+parser.add_argument('--input_size', type=int, default=64, help='input size')
 
 opts = parser.parse_args()
 
